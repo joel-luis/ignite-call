f52bb9bc6be8453024164fc3cb8c1506b30fe08e
Joel Luis <joelluis73@gmail.com>
2023-01-04 20:03:11 -0300

    Create layout home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          alt="ignite call app preview"
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
